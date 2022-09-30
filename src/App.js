cb08c099e4048e972898adffc5b320bc646ef786
Patrick Renz Garcia <trickrenzgarcia@gmail.com>
2022-09-30 13:06:42 +0800

    build 3d react app using React, Threejs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon';
+import { BoxesArray } from './BoxesArray';
+
+// the box component
+export const Box = ({position}) => {
+  const [ref, api] = useBox(() => ({ mass: 1}))
+  return (
+    <mesh onClick={() => {
+      api.velocity.set(0, 10, 0)
+    }} ref={ref} position={position}>
+      <boxGeometry attach="geometry" />
+      <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  )
+}
+
+// the plane component
+export const Plane = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0]
+  }))
+
+  return (
+    <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
+    </mesh>
+  )
+}
 
 function App() {
+  const [boxes, setBoxes] = useState(BoxesArray)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ fov: 75, position: [-20, 15, 20]}}>  
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <Physics>
+        {boxes.map((box) => (
+          <Box key={box.id} position={box.position}/>
+        ))}
+        <Plane />
+      </Physics>
+    </Canvas>
   );
 }
 
---
+++ src/BoxesArray.js
@@ -0,0 +1,14 @@
+export const BoxesArray = [
+  {
+    id: 1,
+    position: [0,5,0]
+  },
+  {
+    id: 2,
+    position: [0,5,0]
+  },
+  {
+    id: 3,
+    position: [0,5,0]
+  },
+]
